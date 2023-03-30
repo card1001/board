a53cc8bdd6e4cde2d0e194f09a1bc83ef5b65d1c
hhd <card1001@hanmail.net>
2023-03-30 23:08:29 +0900

    #5 - 게시글 뷰 테스트 내용 업데이트
    기존 테스트 실패한 채 올리면 `gradle build`가 실패하여 배포 자동화에 악영향을 미치는 경우가 있으므로 , 실패 테스트는 테스트 제외 처리하는 방향으로 정책을 잡아보기로 했다
    이에 기능 구현중인 유닛 테스트를 모두 `@Disabled` 처리
    그밖에 추가로 생각난 테스트 방법을 추가함

--- src/test/java/com/fast/fastboard/controller/ArticleControllerTest.java
+++ src/test/java/com/fast/fastboard/controller/ArticleControllerTest.java
@@ -1,5 +1,6 @@
 package com.fast.fastboard.controller;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +19,7 @@ class ArticleControllerTest {
     public ArticleControllerTest(@Autowired MockMvc mvc){
         this.mvc = mvc;
     }
+    @Disabled("구현중")
     @DisplayName("[view][GET] 게시글 리스트 (게시판)페이지 - 정상호출")
     @Test
     public void givenNothing_whenRequestingArticlesView_thenReturnArticlesView() throws Exception {
@@ -26,9 +28,11 @@ class ArticleControllerTest {
         mvc.perform(get("/articles"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(view().name("/articles/index"))
                 .andExpect(model().attributeExists("articles"));
     }
 
+    @Disabled("구현중")
     @DisplayName("[view][GET] 게시글 상세 페이지 - 정상호출")
     @Test
     public void givenNothing_whenRequestingArticleView_thenReturnArticleView() throws Exception {
@@ -37,9 +41,12 @@ class ArticleControllerTest {
         mvc.perform(get("/articles/1"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.TEXT_HTML))
-                .andExpect(model().attributeExists("article"));
+                .andExpect(view().name("/articles/detail"))
+                .andExpect(model().attributeExists("article"))
+                .andExpect(model().attributeExists("articleComments"));
     }
 
+    @Disabled("구현중")
     @DisplayName("[view][GET] 게시글 검색 전용 페이지 - 정상호출")
     @Test
     public void givenNothing_whenRequestingArticleSearchView_thenReturnArticleSearchView() throws Exception {
@@ -47,9 +54,11 @@ class ArticleControllerTest {
         //WHen & Then
         mvc.perform(get("/articles/search"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML));
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(view().name("/articles/search"));
     }
 
+    @Disabled("구현중")
     @DisplayName("[view][GET] 게시글 해시태그 검색 페이지 - 정상호출")
     @Test
     public void givenNothing_whenRequestingArticleHashtagSearchView_thenReturnArticleHashtagSearchView() throws Exception {
@@ -57,7 +66,8 @@ class ArticleControllerTest {
         //WHen & Then
         mvc.perform(get("/articles/search-hashtag"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML));
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(model().attributeExists("article/search-hashtag"));
     }
 
 }
