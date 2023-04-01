da004040a1b5352e0b7d37587251aa554026b6d2
hhd <card1001@hanmail.net>
2023-04-01 20:23:18 +0900

    #5 -Thymeleaf Config 설정
    설정

--- src/main/java/com/fast/fastboard/FastBoardApplication.java
+++ src/main/java/com/fast/fastboard/FastBoardApplication.java
@@ -2,7 +2,9 @@ package com.fast.fastboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class FastBoardApplication {
 
--- src/main/java/com/fast/fastboard/config/ThymeleafConfig.java
+++ src/main/java/com/fast/fastboard/config/ThymeleafConfig.java
@@ -1,9 +1,14 @@
 package com.fast.fastboard.config;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
 
 @Configuration
-public class ThymeleiafConfig {
+public class ThymeleafConfig {
     @Bean
     public SpringResourceTemplateResolver thymeleafTemplateResolver(
             SpringResourceTemplateResolver defaultTemplateResolver,
@@ -17,7 +22,6 @@ public class ThymeleiafConfig {
 
     @RequiredArgsConstructor
     @Getter
-    @ConstructorBinding
     @ConfigurationProperties("spring.thymeleaf3")
     public static class Thymeleaf3Properties {
         /**
